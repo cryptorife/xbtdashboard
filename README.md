e1c30b5f3e486dfe03d007e0cc63f6a3f9232e7e
cryptorife <cryptorife@pm.me>
2020-03-13 13:46:32 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Grafana BitMex Dashboard
+
+Get the stack (only once):
+
+Run your stack:
+
+```
+docker-compose up -d
+
+```
+
+Problems? Show me the logs:
+
+```
+docker-compose logs
+```
+
+Stop it:
+
+```
+docker-compose stop
+docker-compose rm
+```
+
+Enjoy
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo
+echo "Current database list"
+curl -G http://localhost:8086/query?pretty=true --data-urlencode "db=glances" --data-urlencode "q=SHOW DATABASES"
+
+echo
+echo "Create a new database ?"
+echo "curl -XPOST 'http://localhost:8086/query' --data-urlencode 'q=CREATE DATABASE telegraf'"
